db7ab9b62611b9157c3d17f030971856074e5554
kzauha <kabishpok@gmail.com>
2025-06-21 22:31:05 +0545

    revert

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ class FlaskTest(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/hello')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode(), "Hello, DevOps! K xa?")
+        self.assertEqual(response.data.decode(), "Hello, DevOps!")
 
 if __name__ == '__main__':
     unittest.main()
