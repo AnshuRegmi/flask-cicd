6270e71c3a9870405ca10419e7733c9bc971828a
ashjdbhjasbdjh <73210910+kzauha@users.noreply.github.com>
2025-06-22 07:20:33 +0545

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ class FlaskTest(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/hello')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode(), "Hello, DevOps! k xa?")
+        self.assertEqual(response.data.decode(), "Hello, DevOps!k xa?")
 
 if __name__ == '__main__':
     unittest.main()
